0c78cf17cdd2ae25f01e06eb6d09b41df712e4ea
Кирилл <91612813+jiuper@users.noreply.github.com>
2024-08-11 10:49:14 +0300

    fix

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -1,14 +1,24 @@
 import {Section, List, Button} from '@telegram-apps/telegram-ui';
-import type { FC } from 'react';
+import {FC, useEffect, useState} from 'react';
 
 export const IndexPage: FC = () => {
+    const [value, setValue] = useState([]);
+    useEffect(() => {
+        fetch('https://dikidi-booking-api.onrender.com/api/booking/masters?companyId=591511')
+            .then(response => response.json())
+            .then(json => setValue(json))
+    }, []);
   return (
     <List>
       <Section
         header='Приложение для салона БОГОКО'
         footer='Тестовое приложение которое будем делать'
       >
-asd
+        <ul>
+            {
+                value.map(el => <li key={el}>{el}</li>)
+            }
+        </ul>
           <Button>Отправить</Button>
       </Section>
     </List>
