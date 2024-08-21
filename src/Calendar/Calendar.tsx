bf0061e176fbdf8277cdb2dc039af87d21f165a4
Кирилл <91612813+jiuper@users.noreply.github.com>
2024-08-21 10:31:42 +0300

    fix

--- src/Calendar/Calendar.tsx
+++ src/Calendar/Calendar.tsx
@@ -1,48 +1,47 @@
-import cnBind from "classnames/bind";
-import styles from "./Calendar.module.scss";
 import { useState } from "react";
+import cnBind from "classnames/bind";
 import { DateTime, Info } from "luxon";
-import { eachDayOfInterval, endOfWeek, startOfWeek } from "date-fns";
-import { SvgIcon } from "@/shared/ui/SvgIcon/SvgIcon.tsx";
-const cx = cnBind.bind(styles);
-type Props = {
 
-};
-export const Calendar = (props: Props) => {
-    const [dateTime, setDateTime] = useState<DateTime>(
-        DateTime.now().startOf('day'),
-    );
-     const getDays = () => {
-        return [...Info.weekdays('short')];
-    }
+import { SvgIcon } from "@/shared/ui/SvgIcon/SvgIcon.tsx";
+
+import styles from "./Calendar.module.scss";
+
+const cx = cnBind.bind(styles);
+
+export const Calendar = () => {
+    const [dateTime, setDateTime] = useState<DateTime>(DateTime.now().startOf("day"));
+    const getDays = () => {
+        return [...Info.weekdays("short")];
+    };
     const trans = ["ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ", "ВС"];
     const [days] = useState(getDays());
-    const [initialDate] = useState(dateTime);
-    const date = eachDayOfInterval({ start: startOfWeek(dateTime.toISO(), {
-            weekStartsOn: 1,
-        }), end: endOfWeek(dateTime.toISO(), { weekStartsOn: 1 }) });
-    const arr = date.map((elem) => elem.toLocaleString().slice(0, 10));
-    const onPlusHandler = (type = 'week', value = 1) => {
+    // const [initialDate] = useState(dateTime);
+    //  const date = eachDayOfInterval({ start: startOfWeek(dateTime.toISO(), {
+    //          weekStartsOn: 1,
+    //      }), end: endOfWeek(dateTime.toISO(), { weekStartsOn: 1 }) });
+    // const arr = date.map((elem) => elem.toLocaleString().slice(0, 10));
+    const onPlusHandler = (type = "week", value = 1) => {
         const payload = dateTime.plus({ [type]: value });
         setDateTime(payload);
     };
-    const onMinusHandler = (type = 'week', value = 1) => {
+    const onMinusHandler = (type = "week", value = 1) => {
         const payload = dateTime.minus({ [type]: value });
         setDateTime(payload);
     };
     console.log(dateTime.toLocaleString());
+
     return (
         <div className={cx("calendar")}>
             <div className={cx("buttons")}>
-                <SvgIcon className={cx("prev")} name={"ArrowRight"} onClick={() => onMinusHandler()}/>
-                <SvgIcon className={cx("next")} name={"ArrowRight"} onClick={() => onPlusHandler()}/>
+                <SvgIcon className={cx("prev")} name="ArrowRight" onClick={() => onMinusHandler()} />
+                <SvgIcon className={cx("next")} name="ArrowRight" onClick={() => onPlusHandler()} />
             </div>
 
             <div className={cx("content")}>
                 <div className={cx("days-week")}>
                     {days.map((_, i) => (
                         <div
-                            className={cx('item', {
+                            className={cx("item", {
                                 active: i + 1 === new Date().getDay(),
                             })}
                             key={i}
@@ -51,8 +50,6 @@ export const Calendar = (props: Props) => {
                         </div>
                     ))}
                 </div>
-
-                
             </div>
         </div>
     );
