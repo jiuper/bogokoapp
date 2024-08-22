4b958061636467113775d4e285a8ee0f97f18763
Кирилл <91612813+jiuper@users.noreply.github.com>
2024-08-22 16:23:18 +0300

    fix

--- src/Calendar/Calendar.tsx
+++ src/Calendar/Calendar.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import cnBind from "classnames/bind";
+import { eachDayOfInterval, endOfWeek, startOfWeek } from "date-fns";
 import { DateTime, Info } from "luxon";
 
 import { SvgIcon } from "@/shared/ui/SvgIcon/SvgIcon.tsx";
@@ -10,58 +11,66 @@ const cx = cnBind.bind(styles);
 
 export const Calendar = () => {
     const [dateTime, setDateTime] = useState<DateTime>(DateTime.now().startOf("day"));
-    const getDays = () => {
-        return [...Info.weekdays("short")];
-    };
-    // const asd = [
-    //     "2024-08-22",
-    //     "2024-08-23",
-    //     "2024-08-26",
-    //     "2024-08-27",
-    //     "2024-08-30",
-    //     "2024-08-31"
-    // ]
-    // const asd2 = asd.map(el => el.slice(el.length - 2, el.length))
+    const [selectedDate, setSelectedDate] = useState<Date>(new Date());
+    const getDays = () => [...Info.weekdays("short")];
+
+    const listDate = ["2024-08-22", "2024-08-23", "2024-08-26", "2024-08-27", "2024-08-30", "2024-08-31"];
+    const formatListDate = listDate.map((el) => new Date(el).toLocaleDateString());
     const trans = ["ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ", "ВС"];
     const [days] = useState(getDays());
+    const startWeek = startOfWeek(dateTime.toISO() || new Date(), {
+        weekStartsOn: 1,
+    });
+    const endWeek = endOfWeek(dateTime.toISO() || new Date(), { weekStartsOn: 1 });
+    const date = eachDayOfInterval({
+        start: startWeek,
+        end: endWeek,
+    });
 
-    // const date = eachDayOfInterval({ start: startOfWeek(dateTime.toISO(), {
-    //          weekStartsOn: 1,
-    //      }), end: endOfWeek(dateTime.toISO(), { weekStartsOn: 1 }) });
-    // const arr = date.map((elem) => elem.toLocaleString().slice(0, 10));
-    const onPlusHandler = (type = "week", value = 1) => {
-        const payload = dateTime.plus({ [type]: value });
-        setDateTime(payload);
-    };
-    const onMinusHandler = (type = "week", value = 1) => {
-        const payload = dateTime.minus({ [type]: value });
-        setDateTime(payload);
-    };
+    const onPlusHandler = (type = "week", value = 1) => setDateTime(dateTime.plus({ [type]: value }));
+    const onMinusHandler = (type = "week", value = 1) => setDateTime(dateTime.minus({ [type]: value }));
+    const onSelectHandler = (date: Date) => setSelectedDate(date);
 
     return (
         <div className={cx("calendar")}>
-            <div className={cx("buttons")}>
-                <SvgIcon className={cx("prev")} name="ArrowRight" onClick={() => onMinusHandler()} />
-                <SvgIcon className={cx("next")} name="ArrowRight" onClick={() => onPlusHandler()} />
+            <div className={cx("header")}>
+                <span className={cx("title")}>{dateTime.setLocale("ru").toFormat("LLLL yyyy")}</span>
+                <div className={cx("buttons")}>
+                    <SvgIcon className={cx("prev")} name="ArrowRight" onClick={() => onMinusHandler()} />
+                    <SvgIcon className={cx("next")} name="ArrowRight" onClick={() => onPlusHandler()} />
+                </div>
             </div>
 
             <div className={cx("content")}>
-                <div className={cx("days-week")}>
-                    {days.map((_, i) => (
-                        <div className={cx("item")} key={i}>
-                            {trans[i]}
+                {date.map((day, index) => {
+                    const initDay = day.toLocaleDateString();
+                    const today = new Date().toLocaleDateString();
+                    const numberDay = new Date(day).getDate();
+                    const selectedDay = initDay === selectedDate?.toLocaleDateString();
+
+                    return (
+                        <div className={cx("days")} key={initDay}>
+                            {days.map((_, i) =>
+                                i === index ? (
+                                    <div
+                                        key={day.toLocaleDateString()}
+                                        className={cx("day", {
+                                            work: formatListDate.includes(initDay),
+                                            notWork: !formatListDate.includes(initDay),
+                                            today: initDay === today,
+                                            weekend: new Date(day).getDay() === 0 || new Date(day).getDay() === 6,
+                                            selectedDay,
+                                        })}
+                                        onClick={() => onSelectHandler(day)}
+                                    >
+                                        <span className={cx("day-week")}>{trans[i]}</span>
+                                        <span className={cx("day-number")}>{numberDay}</span>
+                                    </div>
+                                ) : null,
+                            )}
                         </div>
-                    ))}
-                </div>
-                {/* <div className={cx("days")}> */}
-                {/*     { */}
-                {/*         arr.map(day => */}
-                {/*             <div className={cx("day", { active: asd2.includes(day.slice(0,2)) })} key={day}> */}
-                {/*                 {day.slice(0,2)} */}
-                {/*             </div> */}
-                {/*         ) */}
-                {/*     } */}
-                {/* </div> */}
+                    );
+                })}
             </div>
         </div>
     );
